b14fe93f968857045c477fdbd8bc93c5a3c0e859
kairatzhamalbek-cd <kairat.zhamalbek@narxoz.kz>
2026-04-05 11:53:45 +0600

    Add GameScreen and switch to screen-based architecture

--- core/src/main/java/com/pixelforge/combatshift/GameScreen.java
+++ core/src/main/java/com/pixelforge/combatshift/GameScreen.java
@@ -1,4 +1,66 @@
 package com.pixelforge.combatshift;
 
-public class GameScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.pixelforge.combatshift.entity.Player;
+
+public class GameScreen implements Screen {
+
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Player player;
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        player = new Player(100, 100, 200);
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.12f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        player.render(batch);
+        batch.end();
+    }
+
+    private void update(float delta) {
+        player.update(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        player.dispose();
+    }
 }
