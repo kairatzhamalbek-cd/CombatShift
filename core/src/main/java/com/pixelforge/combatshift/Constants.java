7abc9e1a97c466bc06920f0b0864e3a6ce3e9545
kairatzhamalbek-cd <kairat.zhamalbek@narxoz.kz>
2026-05-19 12:16:58 +0600

    Add Constants class for game configuration values

---
+++ core/src/main/java/com/pixelforge/combatshift/Constants.java
@@ -0,0 +1,15 @@
+package com.pixelforge.combatshift;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final float TILE_SIZE = 32f;
+    public static final int MAP_WIDTH_TILES = 100;
+    public static final int MAP_HEIGHT_TILES = 100;
+
+    public static final float WORLD_WIDTH = MAP_WIDTH_TILES * TILE_SIZE;
+    public static final float WORLD_HEIGHT = MAP_HEIGHT_TILES * TILE_SIZE;
+
+    public static final float PLAYER_SIZE = 32f;
+    public static final float PLAYER_SPEED = 250f;   // пикселей в секунду
+}
