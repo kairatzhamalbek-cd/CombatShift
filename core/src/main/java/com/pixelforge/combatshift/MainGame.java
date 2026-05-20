aa875bbe0892114f014e3b8e3c99ffc2f6cdb636
Yertis <yertis.ydyrys@narxoz.kz>
2026-05-20 17:47:28 +0500

    Set MainMenuScreen as the initial screen on game launch

--- core/src/main/java/com/pixelforge/combatshift/MainGame.java
+++ core/src/main/java/com/pixelforge/combatshift/MainGame.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.Game;
 public class MainGame extends Game {
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        setScreen(new MainMenuScreen(this));   // ← Главное меню при запуске
     }
 }
